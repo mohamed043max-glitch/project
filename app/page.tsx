c6fa0f3ddc73328fdc930f93089a02a58bfb909a
mohamed043max-glitch <mohamed043max@gmail.com>
2026-09-21 22:29:40 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>مرحباً بك في موقعي</h1>
+      <p>الموقع يعمل بنجاح على Vercel!</p>
+    </main>
+  );
+}
